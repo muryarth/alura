cda87183244e0d4cc55456bb9a473bb03991e433
Arthur Mury <arthur.mury@outlook.com>
2023-09-19 16:21:31 -0300

    [expo-pet-shop] Criação do componente "servicos"
    Criação do componente que irá listar os principais serviços da app.

---
+++ expo-pet-shop/src/telas/servicos/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { FlatList, SafeAreaView, StatusBar, Text, StyleSheet } from 'react-native';
+import Item from "./item";
+
+const servicos = [
+    {
+        id: 1,
+        nome: "Banho",
+        preco: 79.9,
+        descricao: "NÃO DÊ BANHO NO SEU GATO! Mas se precisar, nós damos."
+    },
+    {
+        id: 2,
+        nome: "Vacina V4",
+        preco: 89.9,
+        descricao: "Uma dose da vacina V4. Seu gato precisa de duas."
+    },
+    {
+        id: 3,
+        nome: "Vacina Antirrábica",
+        preco: 99.9,
+        descricao: "Uma dose da vacina antirrábica. Seu gato precisa de uma."
+    },
+]
+
+export default function Servicos() {
+    return (
+        <SafeAreaView>
+            <StatusBar />
+            <Text>
+                Serviços!
+            </Text>
+            <FlatList
+                data={servicos}
+                //desestruturando
+                renderItem={({ item }) => {
+                    return <Item {...item} />;
+                }}
+                //garante que cada item tenha sua key
+                keyExtractor={({ id }) => String(id)}
+            />
+        </SafeAreaView>
+    )
+}
+
